890f43d7794d888ee1c036a9888fce92df32ae44
Nathan W Johnson <Nathanjohnson482@gmail.com>
2024-09-22 22:20:10 -0500

    initial data processing example

---
+++ data/data_processing.py
@@ -0,0 +1,18 @@
+# import pandas, if you dont have this you should "pip install pandas"
+import pandas as pd
+
+# Load CSV file you found from kaggle and skip corrupted lines (i.e. bad data)
+df = pd.read_csv('ratings.csv.gz', compression='gzip', on_bad_lines='skip')
+
+## If the data you have isnt zipped you could do this
+# df = pd.read_csv('ratings.csv', on_bad_lines='skip')
+
+## Display the loaded DataFrame with some sample methods
+# The empty print() statement are for empty line spacing
+print(df.head(5))
+print()
+print(df.tail(5))
+print()
+print(df.info)
+print()
+print(df.shape)
