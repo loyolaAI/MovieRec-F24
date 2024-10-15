12d1b0cd0be684e9b4bab8234f39f538884014b1
Nathan W Johnson <Nathanjohnson482@gmail.com>
2024-10-14 23:17:13 -0500

    First implementation of model completed

---
+++ model/main.py
@@ -0,0 +1,35 @@
+from get_user_data import get_movie_dataframe
+from build_colab_model import build_colab_model
+from run_colab_model import run_colab_model
+from scraping import scrape_and_make_dataframe
+
+
+def main():
+    # Get the accuracy to see how much data we want to use for the model and recommendations.
+    accuracy = float(input("How accurate do you want the model to be (range 0.01-1.0) "))
+
+    # Call get_movie_dataframe to get all the user data and ratings to make recommendations.
+    df = get_movie_dataframe(accuracy)
+
+    # Get the users, letterbox'd username, to scrape ratings.
+    username = input("What is your letterboxd username ")
+    user_data = scrape_and_make_dataframe(username)
+
+    # Ask the user for how many movies recommendations they want
+    number_recs = int(input("How many recommendations do you want "))
+
+    # build the collaborative based model and return
+    algo = build_colab_model(df, user_data, accuracy)
+
+    # Run the collaborative based model and return the recommendations
+    recs = run_colab_model(algo, user_data, df, accuracy, username, number_recs)
+
+    # Print the recommended items for the user
+    for rec in recs:
+        movie_name = rec.get("film_id")
+        rating = rec.get("unclipped_rating")
+        print(f"{movie_name} {round(rating, 2)}")
+
+
+if __name__ == "__main__":
+    main()
--- model/run_colab_model.py
+++ model/run_colab_model.py
@@ -1,110 +1,82 @@
-# Most likely won't need all of these, but will need at least some
-from collections import defaultdict
-
-from surprise import Dataset
-from surprise import SVD
-from surprise import Reader
-from surprise.model_selection import GridSearchCV
-from surprise import SVD
-from surprise.dump import load
-
-import pickle
 import pandas as pd
 import random
 
-import os
-
 from build_colab_model import build_colab_model
-from get_user_data import get_unwatched_movies
 from scraping import scrape_and_make_dataframe
-
-"""
-FUNCTION get_top_recs(predictions, n=20):
-    top_rec = CREATE list of tuples (movie_id, estimated_rating) from predictions
-    SORT top_rec by estimated_rating and a random tiebreaker, in descending order
-    RETURN the top n items from top_rec
-"""
 
 
 def get_top_recs(predictions, num_recs):
+    # top_n is a list of tuples that has the movie name and the predicted rating
     top_n = [(iid, est) for uid, iid, true_r, est, _ in predictions]
+    # Sort top_n by the predicted rating, sorting from highest to lowest.
     top_n.sort(key=lambda x: (x[1], random.random()), reverse=True)
-
+    # return the spliced list of the top_n recommendations
     return top_n[:num_recs]
 
 
-def run_colab_model(algo, user_data, movies, accuracy: float, username, num_recs=25):
-    """
-    WE need to load the .pkl file and run the model on movies the given user has not seen
-    """
+def run_colab_model(algo, user_data, movies, accuracy, username, num_recs=25):
+    """We need to get all the movies the user has not watched"""
+    # Get a set of movie IDs that the user has already watched
+    watched_movies = set(user_data["film_id"].unique())
 
-    # Identify movies the user has not watched, type list.
-    print("#### Get all the movies the user has not watched")
-    unwatched_movies = get_unwatched_movies(user_data, movies, accuracy)
-    print(unwatched_movies[:10])
+    # Get a set of all movie IDs
+    all_movies = set(movies["film_id"].unique())
+
+    # Get the difference: movies the user hasn't watched
+    unwatched_movies = list(all_movies - watched_movies)
 
     # Create the prediction set which will hold a given users film and the predicted rating for it.
     prediction_set = [(username, film_id, 0) for film_id in unwatched_movies]
 
-    # predict!
+    # Feed the prediction_set into the model. This returns a list of type
+    # 'surprise.prediction_algorithms.predictions.Prediction' which is basically a dictionary
+    # With types | user: <val> item: <movie_name> r_ui = <true_r> est = <> {'was_impossible': True/False}
     predictions = algo.test(prediction_set)
-    print("#### Get all the movies rating predictions from the model")
-    print("#### Sort and get the top recommendations")
-    print("#### print out the recommendations")
+
+    # We then feed that pseudo dictionary into get_top_recs, which basically takes the values that we care
+    # about, and sorts them to get the highest rated predictions, it then returns n highest rated predictions
     top_recs = get_top_recs(predictions, num_recs)
-    # Print the recommended items for user
-    for prediction in top_recs:
-        print(f"{prediction[0]}: {round(prediction[1], 2)}")
 
-    return "lol"
+    # Define the returns object which is a list of dictionaries, the dictionary stores the film_id, the initial
+    # predicted rating, and then the unclipped rating, which is explained below
+    res = [{"film_id": x[0], "predicted_rating": x[1], "unclipped_rating": x[1]} for x in top_recs]
 
-    # Get predictions from the algorithm
-    # predictions = TEST algorithm using prediction_set
-    # predictions = algo.test(prediction_set)
-    # Call the get_top_recs to get the highest rated recommendations
-    # top_recs = get_top_recs(predictions, num_recommendations)
+    """ Because we clip the rating in the range 1-10, to match the star ratings, if we have a rating that is 
+        equal to 10, we need to repredict to see what the unclipped rating is for the given movie. For most 
+        cases, the predicted_rating == unclipped_rating. """
+    for i, prediction in enumerate(res):
+        if prediction["predicted_rating"] == 10:
+            res[i]["unclipped_rating"] = float(
+                algo.predict(username, prediction["movie_id"], clip=False).est
+            )
 
-    # Get movie details from "movies.csv.gz" file
-    # Using the movie name we got recommended, we can pull info from the csv we have
-    # to get the year, actors, ect
-    # res = more_movie_info
+    # Sort the return array by the unclipped rating, and then return
+    res.sort(key=lambda x: (x["unclipped_rating"]), reverse=True)
 
-    # Because the way scikit-learn SVD works we may need to adjust the ratings
-    # Adjust the unclipped rating if necessary
-    # FOR each prediction in return_object:
-    #     IF predicted_rating == 10:
-    #         ADJUST unclipped_rating using algorithm.predict with clipping disabled
-
-    # Sort the return object by unclipped_rating in descending order
-    # SORT res by unclipped_rating in descending order
-    # RETURN return_object
+    return res
 
 
+# Sample use case
 if __name__ == "__main__":
-    """
-    algo = Open the model file (.pkl)
-    movies = Open the list of movies (data/films.csv.gz)
-    Get the movies the user has watched
-    Normally we would scrap the data and then call the "get_movie_info" method
-    Within get_user_data.py for this data, instead we use the sample data.
-    user_data = pd.read_csv("./data/sample_user_data.csv")
-    recommendations = run_colab_model(algo, user_data, movies)
-    print(recommendation)
-    """
+    # Define accuracy to something small, since this is a just a sample use case
+    accuracy = 0.05
 
-    accuracy = float(input("How accurate do you want the model to be (range 0.01-1.0) "))
-    # Normally we would scrap the data and then call the "get_movie_info" method
-    # Within get_user_data.py for this data, instead we use the sample data.
-    username = input("What is your letterboxd username ")
-    print("#### scraping User data")
-    user_data = scrape_and_make_dataframe(username)
-    print(user_data)
-    print("#### Scraping user data completed")
-    print("#### Loading in movies dataset")
-    df = pd.read_csv("./data/ratings.csv.gz", compression="gzip")
-    print(df.head(10))
-    print("#### Done loading movies dataset")
-    print("#### Build the colab model")
+    # Call get_movie_dataframe to get all the user data and ratings to make recommendations.
+    from get_user_data import get_movie_dataframe
+
+    df = get_movie_dataframe(accuracy)
+
+    # using sample data
+    user_data = pd.read_csv("./data/sample_user_data.csv")
+
+    # build the collaborative based model and return
     algo = build_colab_model(df, user_data, accuracy)
-    print("#### Run the colab model")
-    recs = run_colab_model(algo, user_data, df, accuracy, username, 20)
+
+    # Run the collaborative based model and return the recommendations
+    recs = run_colab_model(algo, user_data, df, accuracy, "nathannjohnson", 20)
+
+    # Print the recommended items for the user
+    for rec in recs:
+        movie_name = rec.get("film_id")
+        rating = rec.get("unclipped_rating")
+        print(f"{movie_name} {round(rating, 2)}")
